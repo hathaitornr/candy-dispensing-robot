f18cc2857151ce8a55a2a05f9bfe5b5254b0fc0c
darenl <drl322@nyu.edu>
2018-05-06 20:17:05 -0400

    uncommented parts

--- neato-server.js
+++ neato-server.js
@@ -19,18 +19,18 @@ io.on('connection', function(socket){
   
   socket.on('info', function(data){
     console.log("info:", data);
-    //drive(10,10,100);
+    drive(10,10,100);
   });
 
   //drive messages
   socket.on('drive', function(data){
     console.log("drive:", data);
-    //drive(data.LWheelDist, data.RWheelDist, data.Speed);
+    drive(data.LWheelDist, data.RWheelDist, data.Speed);
   });
 
   socket.on('move', function(direction){
     console.log(direction);
-    //driveKey({'name': direction});
+    driveKey({'name': direction});
   });
 
   socket.on('disconnect', function(){});
@@ -38,79 +38,79 @@ io.on('connection', function(socket){
 
 
 // make `process.stdin` begin emitting "keypress" events
-//keypress(process.stdin);
+keypress(process.stdin);
 
-//// listen for the "keypress" event
-// process.stdin.on('keypress', function (ch, key) {
-//   //console.log('got "keypress"', key);
-//   driveKey(key);
-//   if (key && key.ctrl && key.name == 'c') {
-//     process.stdin.exit();
-//   }
-// });
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  console.log('got "keypress"', key);
+  driveKey(key);
+  if (key && key.ctrl && key.name == 'c') {
+    process.stdin.exit();
+  }
+});
 
-// process.stdin.setRawMode(true);
-// process.stdin.resume();
+process.stdin.setRawMode(true);
+process.stdin.resume();
 
-//// Setup the connection to the Neato
-// var port = new SerialPort("/dev/ttyACM0", {
-//   baudRate: 115200
-// });
+// Setup the connection to the Neato
+var port = new SerialPort("/dev/ttyACM0", {
+  baudRate: 115200
+});
 
-// // Open the port and put the robot into testmode ON so we can drive
-// port.on('open', function() {
-//   port.write('testmode on' + '\n', function(err) {
-//     if (err) {
-//       return console.log('Error on write: ', err.message);
-//     }
-//     console.log('Neato Ready!');
-//   });
+// Open the port and put the robot into testmode ON so we can drive
+port.on('open', function() {
+  port.write('testmode on' + '\n', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+    console.log('Neato Ready!');
+  });
 
-//   //testPath();
-// });
+  //testPath();
+});
 
-// // open errors will be emitted as an error event
-// port.on('error', function(err) {
-//   console.log('Error: ', err.message);
-// })
+// open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message);
+})
 
-// // drive the robot on a sample path
-// function testPath() {
-//   port.write('SetMotor LWheelDist 500 RWheelDist 500 Speed 50' + '\n');
-// }
+// drive the robot on a sample path
+function testPath() {
+  port.write('SetMotor LWheelDist 500 RWheelDist 500 Speed 50' + '\n');
+}
 
-// // drive the robot from messsages
-// function drive(LWheelDist, RWheelDist, Speed) {
-//   console.log('SetMotor LWheelDist ' + LWheelDist + 
-//              ' RWheelDist ' + RWheelDist + ' Speed ' + Speed + '\n')
-//   port.write('SetMotor LWheelDist ' + LWheelDist + 
-//              ' RWheelDist ' + RWheelDist + ' Speed ' + Speed + '\n');
-// }
+// drive the robot from messsages
+function drive(LWheelDist, RWheelDist, Speed) {
+  console.log('SetMotor LWheelDist ' + LWheelDist + 
+             ' RWheelDist ' + RWheelDist + ' Speed ' + Speed + '\n')
+  port.write('SetMotor LWheelDist ' + LWheelDist + 
+             ' RWheelDist ' + RWheelDist + ' Speed ' + Speed + '\n');
+}
 
 
-// //drive the robot with keypresses
-// function driveKey(key) {
-//   var dist = 20;
-//   switch (key.name) {
-//     case 'w': //fwd
-//       port.write('SetMotor LWheelDist 20 RWheelDist 20 Speed 100' + '\n');
-//       break;
-//     case 'a': //left
-//       port.write('SetMotor LWheelDist -20 RWheelDist 20 Speed 100' + '\n');
-//       break;
-//     case 's': //back
-//       port.write('SetMotor LWheelDist -20 RWheelDist -20 Speed 100' + '\n');
-//       break;
-//     case 'd': //right
-//       port.write('SetMotor LWheelDist 20 RWheelDist -20 Speed 100' + '\n');
-//       break;
-//     case 'q': //slight - left
-//       port.write('SetMotor LWheelDist 10 RWheelDist 20 Speed 100' + '\n');
-//       break;
-//     case 'e': //slight - right
-//       port.write('SetMotor LWheelDist 20 RWheelDist 10 Speed 100' + '\n');
-//       break;
-//     //default:
+//drive the robot with keypresses
+function driveKey(key) {
+  var dist = 20;
+  switch (key.name) {
+    case 'w': //fwd
+      port.write('SetMotor LWheelDist 20 RWheelDist 20 Speed 100' + '\n');
+      break;
+    case 'a': //left
+      port.write('SetMotor LWheelDist -20 RWheelDist 20 Speed 100' + '\n');
+      break;
+    case 's': //back
+      port.write('SetMotor LWheelDist -20 RWheelDist -20 Speed 100' + '\n');
+      break;
+    case 'd': //right
+      port.write('SetMotor LWheelDist 20 RWheelDist -20 Speed 100' + '\n');
+      break;
+    case 'q': //slight - left
+      port.write('SetMotor LWheelDist 10 RWheelDist 20 Speed 100' + '\n');
+      break;
+    case 'e': //slight - right
+      port.write('SetMotor LWheelDist 20 RWheelDist 10 Speed 100' + '\n');
+      break;
+    //default:
 
-//   }
-// }
+  }
+}
